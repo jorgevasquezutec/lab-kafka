d39b52dd93a4ebf3471faef290855f355f77a4fa
Jorge Vásquez <jorge.vasquez@utec.edu.pe>
2022-11-16 18:32:26 -0500

    feat(init) lab kafka,missing model to predict points

---
+++ consumer.py
@@ -0,0 +1,107 @@
+
+from confluent_kafka import Consumer, TopicPartition
+import redis
+import json
+import matplotlib.pyplot as plt
+import time
+from pluck import pluck
+import redis
+from bson.json_util import dumps
+import numpy as np 
+
+# REDIS_HOST = '0.0.0.0'
+# REDIS_PORT = '6379'
+# redis = redis.Redis(host=self.redis_host,port=self.redis_port, decode_responses=True)
+
+
+'''
+    Topic: Weather
+    partitions: 3
+    fr: 2
+
+    p0: d00,d01,d02(commit = True)
+    p1: d10,d11(commit = True) 
+    p2: d20,d21,d22(commit=True),d23(commit=True)
+
+    d = poll(1seg) -> d11, d23, d22
+
+'''
+
+class DataCapture():
+    def __init__(self) -> None:
+        self.conf = {
+            'bootstrap.servers': 'localhost:29092',
+            'group.id': 'test10',     
+            'enable.auto.commit': 'false',
+            'auto.offset.reset': 'earliest',
+            'max.poll.interval.ms': '500000',
+            'session.timeout.ms': '120000',
+            'request.timeout.ms': '120000'
+        }
+        self.redisClient = redis.StrictRedis(host='0.0.0.0', port=6379, db=0)
+        self.redisClient.delete('data')
+
+    def consume(self, topic='test'):
+        self.consumer = Consumer(self.conf)
+        self.topic = topic
+        self.consumer.subscribe([self.topic])
+        self.lastIndex = '+'
+
+        try:
+            while True:
+                # msg = self.consumer.poll(1.0) # consume(100, 1.0)
+                msgs = self.consumer.consume(100, 1.0)                
+                if msgs is None or len(msgs) == 0:
+                    continue
+
+                # events = []
+                start= True
+                for msg in msgs:
+                    event = msg.value()
+                    partition = msg.partition()
+                    offset = msg.offset()                                                         
+                    if event is not None:
+                        # process it
+                        parseEvent = json.loads(event.decode('utf-8'))
+                        if(start):
+                            self.lastIndex=self.redisClient.xadd('data', parseEvent)
+                            start = False
+                        else:
+                            self.redisClient.xadd('data', parseEvent)
+                        # events.append(parseEvent)
+                        
+                    self.consumer.commit(offsets=[TopicPartition(topic = self.topic, partition=partition, offset=offset+1)], asynchronous = False)
+                
+                #print(events)
+                # temperatures = pluck(events,'Temperature')
+                # print(temperatures)
+                data = self.redisClient.xrange('data',min=self.lastIndex,max='+')
+                temperatures =[float(x[1].get(b'Temperature')) for x in  data]
+                vstd = np.std(temperatures)
+                vmedia = np.mean(temperatures)
+                vsdt = np.var(temperatures)
+                print("len: ",len(temperatures))
+                print("std: ",vstd)
+                print("media: ",vmedia)
+                print("varianza: ",vsdt)
+                # irradiances = events.pluck("GHI")
+                # times = events.pluck("Date")
+                plt.plot(temperatures, color='red')
+                # # plt.plot(irradiances, color='blue')
+                plt.ylabel('Temperature')
+                # 
+                plt.show()
+                time.sleep(5)
+
+
+
+                print("==============================")
+
+        except KeyboardInterrupt:
+            print("interrupted error ")
+            self.consumer.close()
+            # plt.close()
+
+
+capture = DataCapture()
+capture.consume('test') 
---
+++ producer.py
@@ -0,0 +1,45 @@
+from confluent_kafka import Producer
+import pandas as pd
+import socket
+import json
+import time
+
+# read dataset "Solargis_min15_Almeria_spain.csv"
+# sample_size = 100
+
+class App():
+    def __init__(self) -> None:
+        self.topic = "test"
+        self.conf_producer = {
+            'bootstrap.servers': 'localhost:29092',        
+            'client.id': socket.gethostname(),
+        }
+        self.producer = Producer(self.conf_producer)
+
+    
+    def produce(self, data):
+        self.producer.produce(self.topic, key=None, value=data)
+        self.producer.flush()
+
+
+def produce_dataframe(df):
+    for row in df.values:
+        value = {"Date": row[0], "GTI": row[2], "Temperature": row[8]}
+        print(value)
+        app.produce(json.dumps(value))
+        time.sleep(.25)
+
+def produce_dataframe_by_chunks(df):
+    for chunk_df in df:
+        produce_dataframe(chunk_df)
+
+
+app = App()
+
+# df = pd.read_csv('../datasets/csv/Solargis_min15_Almeria_Spain.csv')
+
+df100 = pd.read_csv('./datasets/Solargis_min15_Almeria_Spain.csv', chunksize=100)
+
+produce_dataframe_by_chunks(df100)
+
+# app.produce('data-from-produce python api')
